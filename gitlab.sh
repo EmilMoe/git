55eaefc89e53b3260f9749de5bbfabe02f313ed4
Emil Moe <emil@moegroup.dk>
2020-06-02 16:17:25 +0200

    Update gitlab.sh

--- gitlab.sh
+++ gitlab.sh
@@ -16,14 +16,14 @@ debug() {
 # Validate parameters
 payload=$1
 [ -z "$payload" ] && die "missing request payload"
-payload_type=$(echo $payload | jq type -r)
+payload_type=$(echo "$payload" | jq type -r)
 [ $? != 0 ] && die "bad body format: expecting JSON"
-[ ! $payload_type = "object" ] && die "bad body format: expecting JSON object but having $payload_type"
+[ ! "$payload_type" = "object" ] && die "bad body format: expecting JSON object but having $payload_type"
 
 debug "received payload: $payload"
 
 # Extract values
-object_kind=$(echo $payload | jq .object_kind -r)
+object_kind=$(echo "$payload" | jq .object_kind -r)
 [ $? != 0 -o "$object_kind" = "null" ] && die "unable to extract 'object_kind' from JSON payload"
 
 # Do something with the payload:
